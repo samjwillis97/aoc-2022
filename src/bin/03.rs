f1966ab8791373bb1aa076b5bf7bf273f9c1a5aa
Sam Willis <sam@williscloud.org>
2022-12-04 12:42:02 +1100

    day3 completed.

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,4 +1,6 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+
+use itertools::Itertools;
 
 // with the let mut sets in the map, 360us
 // by initing them outside and draining in the map 260us
@@ -29,8 +31,40 @@ pub fn part_one(input: &str) -> Option<u32> {
     )
 }
 
+// 800 us
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut group_map = HashMap::new();
+    let mut group_keys: Vec<u32> = Vec::new();
+
+    input.lines().enumerate().for_each(|(i, elf)| {
+        let mut elf_map = HashSet::new();
+        elf.chars().for_each(|item| {
+            elf_map.insert(convert_char_to_priority(item));
+        });
+        group_map.insert(i, elf_map);
+    });
+
+    // Would be nice to find a better way of doing this
+    for i in (0..input.lines().count()).step_by(3) {
+        let mut union_set = HashSet::new();
+
+        group_map
+            .get(&i)
+            .unwrap()
+            .intersection(group_map.get(&(&i + 1)).unwrap())
+            .for_each(|value| {
+                union_set.insert(*value);
+            });
+
+        group_keys.push(
+            *union_set
+                .intersection(group_map.get(&(&i + 2)).unwrap())
+                .next()
+                .unwrap(),
+        );
+    }
+
+    Some(group_keys.iter().sum())
 }
 
 fn convert_char_to_priority(letter: char) -> u32 {
@@ -59,6 +93,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 3);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(70));
     }
 }
