222a1dde78ba0900753a56d2ffccbe3d843102b5
Sam Willis <sam@williscloud.org>
2022-12-10 09:18:20 +1100

    playing around with day07

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,9 +1,12 @@
 use std::collections::HashMap;
 
+use itertools::Itertools;
+
 pub fn part_one(input: &str) -> Option<u32> {
     let mut file_system = Directory {
         files: HashMap::new(),
         directories: HashMap::new(),
+        total_file_size: 0,
     };
 
     let mut current_location: Vec<String> = Vec::new();
@@ -32,6 +35,7 @@ pub fn part_one(input: &str) -> Option<u32> {
                             Directory {
                                 files: HashMap::new(),
                                 directories: HashMap::new(),
+                                total_file_size: 0,
                             },
                         );
                     }
@@ -39,23 +43,26 @@ pub fn part_one(input: &str) -> Option<u32> {
                 }
 
                 if line[0] != "dir" {
-                    previous_dir
-                        .files
-                        .insert(line[1].to_string(), line[0].parse::<u32>().unwrap());
+                    let size = line[0].parse::<u32>().unwrap();
+                    previous_dir.files.insert(line[1].to_string(), size);
+                    previous_dir.total_file_size = previous_dir.total_file_size + size;
                 }
             }
         }
     });
 
+    println!("{:?}", file_system.directories.get("/").unwrap());
     let dir_sizes = recurse_get_size(file_system.directories.get("/").unwrap());
-    Some(dir_sizes.iter().filter(|size| *size <= &100000).sum())
+    println!("{:?}", dir_sizes);
+    // Some(dir_sizes.iter().filter(|size| *size <= &100000).sum())
+    None
 }
 
 fn recurse_get_size(dir: &Directory) -> Vec<u32> {
     let mut sizes: Vec<u32> = Vec::new();
-    let file_sizes = dir.files.values().sum();
     if dir.directories.len() == 0 {
-        sizes.push(file_sizes);
+        sizes.push(dir.total_file_size);
+        println!("no sub: {}", dir.total_file_size);
         return sizes;
     }
 
@@ -64,21 +71,85 @@ fn recurse_get_size(dir: &Directory) -> Vec<u32> {
     }
 
     let current_sub_sizes: u32 = sizes.iter().sum();
-    sizes.push(file_sizes + current_sub_sizes);
+    println!("sub: {}", current_sub_sizes);
+    println!("current: {}", dir.total_file_size);
+    println!("sum: {}", dir.total_file_size + current_sub_sizes);
+    sizes.push(dir.total_file_size + current_sub_sizes);
 
     return sizes;
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
+    let mut file_system = Directory {
+        files: HashMap::new(),
+        directories: HashMap::new(),
+        total_file_size: 0,
+    };
+
+    let mut current_location: Vec<String> = Vec::new();
+
+    input.lines().for_each(|line| {
+        let line: Vec<&str> = line.split(" ").collect();
+        match line[0] {
+            "$" => match line[1] {
+                "cd" => match line[2] {
+                    ".." => {
+                        current_location.pop();
+                    }
+                    v => {
+                        current_location.push(v.to_string());
+                    }
+                },
+                _ => (),
+            },
+            _ => {
+                // I hate this but seems to create the directories for me..
+                let mut previous_dir = &mut file_system;
+                for dir in &current_location {
+                    if !previous_dir.directories.contains_key(dir) {
+                        previous_dir.directories.insert(
+                            dir.to_string(),
+                            Directory {
+                                files: HashMap::new(),
+                                directories: HashMap::new(),
+                                total_file_size: 0,
+                            },
+                        );
+                    }
+                    previous_dir = previous_dir.directories.get_mut(dir).unwrap();
+                }
+
+                if line[0] != "dir" {
+                    let size = line[0].parse::<u32>().unwrap();
+                    previous_dir.files.insert(line[1].to_string(), size);
+                    previous_dir.total_file_size = previous_dir.total_file_size + size;
+                }
+            }
+        }
+    });
+
+    let mut dir_sizes = recurse_get_size(file_system.directories.get("/").unwrap());
+    dir_sizes.sort();
+
+    println!("{:?}", file_system.directories.get("/").unwrap());
+
+    println!("{:?}", dir_sizes);
+
+    // There are some wrong values in here.. but might be ok
+    let used = dir_sizes.last().unwrap();
+    println!("{:?}", used);
+    let unused = 70000000 - used;
+    println!("{:?}", unused);
+    let required = 30000000 - unused;
+    println!("{:?}", required);
     None
 }
-
-fn handle_command(input: &str) {}
 
 #[derive(Debug)]
 struct Directory {
     files: HashMap<String, u32>,
     directories: HashMap<String, Directory>,
+    total_file_size: u32,
 }
 
 fn main() {
@@ -100,6 +171,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 7);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(24933642));
     }
 }
